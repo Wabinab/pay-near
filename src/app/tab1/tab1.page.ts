d521e4e6ed0f5cc477bd18d8b83c16bba4ca6559
Wabinab <71509699+Wabinab@users.noreply.github.com>
2023-12-18 07:31:10 +0000

    first test

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NFC, NFCOriginal, Ndef, NdefOriginal } from '@awesome-cordova-plugins/nfc';
+import { NFC, Ndef } from '@awesome-cordova-plugins/nfc/ngx';
+import { ToastController } from '@ionic/angular';
 
 
 @Component({
@@ -9,9 +10,14 @@ import { NFC, NFCOriginal, Ndef, NdefOriginal } from '@awesome-cordova-plugins/n
 })
 export class Tab1Page {
 
-  constructor(private nfc: NFCOriginal, private ndef: NdefOriginal) {}
+  tag_info: any;
+  readerMode$: any;
+  discoveredListenerSub$: any;
 
-  test() {
+  constructor(private nfc: NFC, private ndef: Ndef, 
+    private toastCtrl: ToastController) {}
+
+  test_as_sharer() {
     var message = [
       this.ndef.textRecord("hello, world")
     ];
@@ -20,7 +26,75 @@ export class Tab1Page {
     }, err => this.doErr(err));
   }
 
+  ionViewWillEnter() {
+    this.refresh();
+  }
+  ionViewWillLeave() {
+    this.readerMode$.unsubscribe();
+    this.discoveredListenerSub$.unsubscribe();
+  }
+
+  refresh() {
+    this.start_nfc_listener();
+    this.start_nfc_reader_mode();
+  }
+  async handle_refresh(event: any) {
+    const finish = await this.refresh();
+    event.target.complete();
+  }
+
+  // =================================================================
+  // NFC functions
+  start_nfc_reader_mode() {
+    let flags = this.nfc.FLAG_READER_NFC_A | this.nfc.FLAG_READER_NFC_V;
+    this.readerMode$ = this.nfc.readerMode(flags).subscribe(
+      tag => {
+        // this.nfc.bytesToHexString(tag.id!.reverse());
+        this.tag_info = tag;
+        // this.present_toast(`Reader Mode: ${tag}`, "top", "bg-success");
+      }, err => {
+        this.present_toast(`Failed to start NFC reader mode, error: ${err}`, "top", "bg-danger");
+      }
+    )
+  }
+
+  start_nfc_listener() {
+    this.discoveredListenerSub$ = this.nfc.addTagDiscoveredListener(() => {
+      // Do nothing
+    }, (err: any) => {
+      this.present_toast(`Error attaching tag discovered listener: ${err}`, "bottom", 'bg-danger');
+    }).subscribe(event => {
+      this.present_toast(`Received tag discovered listener mesage: ${event.tag}`, "bottom");
+      const tag = this.nfc.bytesToHexString(event.target.id);
+      this.present_toast(`Decoded tag: ${tag}`, "bottom");
+      this.tag_info = event;
+    })
+  }
+
+  // =================================================================
   doErr(err: any) {
     console.error(err);
+  }
+
+  async present_toast(message: string, position: any, 
+    color: string | null = null,
+    duration: number = 1500
+  ) {
+    const toast = await this.toastCtrl.create({
+      message: message,
+      duration: duration,
+      position: position,
+      cssClass: color != null ? `custom-${color}` : '',
+      // cssClass: 'custom-bg-info',
+      buttons: [
+        {
+          text: 'X',
+          role: 'cancel',
+          handler: () => {}
+        }
+      ]
+    });
+
+    await toast.present();
   }
 }
