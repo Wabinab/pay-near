439d829765e274d5c2f39397706c63045b45f23d
Wabinab <71509699+Wabinab@users.noreply.github.com>
2023-12-21 08:54:38 +0000

    basic complete

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { setupWalletSelector } from "@near-wallet-selector/core";
+import { setupMyNearWallet } from "@near-wallet-selector/my-near-wallet";
+import { setupLedger } from "@near-wallet-selector/ledger";
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
 
   constructor() {}
+
+  async ngOnInit() {
+    const selector = await setupWalletSelector({
+      network: "testnet",
+      modules: [
+        setupMyNearWallet(),
+        setupLedger()
+      ],
+    });
+  }
 
 }
