7ca5daedc4c6b487642f95eef0bf1a9e47412902
Wabinab <71509699+Wabinab@users.noreply.github.com>
2023-12-21 09:17:35 +0000

    cannot success for unknown reason

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { setupWalletSelector } from "@near-wallet-selector/core";
 import { setupMyNearWallet } from "@near-wallet-selector/my-near-wallet";
 import { setupLedger } from "@near-wallet-selector/ledger";
+import { setupModal } from '@near-wallet-selector/modal-ui';
 
 @Component({
   selector: 'app-tab3',
@@ -10,16 +11,42 @@ import { setupLedger } from "@near-wallet-selector/ledger";
 })
 export class Tab3Page implements OnInit {
 
+  selector: any = null;
+  modalWallet: any;
   constructor() {}
 
+  // const selector = 
+
   async ngOnInit() {
-    const selector = await setupWalletSelector({
+    await this.setup().catch(err => {
+      console.error(err);
+    });
+  }
+
+  async setup() {
+    await setupWalletSelector({
       network: "testnet",
       modules: [
-        setupMyNearWallet(),
-        setupLedger()
-      ],
+        // setupMyNearWallet(),
+        // setupLedger()
+      ]
     });
+  }
+
+  show_wallet() {
+    // setupWalletSelector({
+    //   network: "testnet",
+    //   modules: [
+    //     setupMyNearWallet(),
+    //     setupLedger()
+    //   ],
+    // }).then(selector => {
+    //   this.modalWallet = setupModal(selector, {
+    //     contractId: 'guest-book.testnet'
+    //   });
+    //   this.modalWallet.show();
+    // })
+    // this.modalWallet.show();
   }
 
 }
