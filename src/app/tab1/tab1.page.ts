8310cc22c8009149390327fca068aa7385296a08
Wabinab <71509699+Wabinab@users.noreply.github.com>
2023-12-21 01:29:12 +0000

    basic scanner test

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,28 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
+  barcode: any;
   constructor() {}
 
+  async request_permission() {
+    await BarcodeScanner.requestPermissions();
+  }
+
+  async start_scan() {
+    await this.request_permission();
+    document.querySelector('body')?.classList.add('barcode-scanning-active');
+
+    const listener = await BarcodeScanner.addListener(
+      "barcodeScanned",
+      async (result) => { this.barcode = result.barcode; }
+    );
+
+    await BarcodeScanner.startScan();
+  }
+
+  async stop_scan() {
+    document.querySelector("body")?.classList.remove('barcode-scanner-active');
+    await BarcodeScanner.removeAllListeners();
+    await BarcodeScanner.stopScan();
+  }
 }
