355879ce8331856249439bf7efd68fcdb83be032
Wabinab <71509699+Wabinab@users.noreply.github.com>
2024-01-16 02:49:21 +0000

    checkpoint 1

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,9 +1,7 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
-import { setupWalletSelector } from "@near-wallet-selector/core";
-import { setupMyNearWallet } from "@near-wallet-selector/my-near-wallet";
-// import { setupLedger } from "@near-wallet-selector/ledger";
-import { setupModal } from '@near-wallet-selector/modal-ui';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { LoginWalletService } from '../services/login-wallet.service';
+import { utils } from 'near-api-js';
+import { Swiper } from 'swiper/types';
 
 @Component({
   selector: 'app-tab3',
@@ -30,11 +28,69 @@ export class Tab3Page implements OnInit {
       "account": this.account_id
     });
 
-    console.warn(this.stats_activated);
+    // console.warn(this.stats_activated);
   }
 
   get account_id() {
     return this.walletSvc.account_id ?? "";
+  }
+
+
+
+  // ================================================
+  activate_stats() {
+    this.walletSvc.call("activate_statistics", {}, 
+      utils.format.parseNearAmount("0.75") ?? "0"
+    );
+  }
+
+  // =================================================
+  // Segments
+  segments = ["spendings", "earnings"];
+  curr_seg = this.segments[0];
+  // on_seg_click(segment: string) {
+  //   this.curr_seg = segment;
+  // }
+
+  // seg_index = 1;
+  // swipe_event(event: any) {
+  //   // Swipe left
+  //   if (event.direction == 2) {
+  //     this.seg_index -= 1;
+  //   }
+
+  //   // Swipe right
+  //   if (event.direction == 4) {
+  //     this.seg_index += 1;
+  //   }
+
+  //   // To prevent overswipe.
+  //   if (this.seg_index > this.segments.length) {
+  //     this.seg_index = this.segments.length;
+  //   }
+  //   if (this.seg_index < 0) {
+  //     this.seg_index = 0;
+  //   }
+
+  //   this.curr_seg = this.segments[this.seg_index];
+  // }
+
+  @ViewChild('swiper')
+  swiperRef: ElementRef | undefined;
+  swiper?: Swiper;
+  
+  swiper_ready() {
+    this.swiper = this.swiperRef?.nativeElement.swiper;
+  }
+
+  swiper_slided(event: any) {
+    const index = event.target.swiper.activeIndex;
+    this.curr_seg = this.segments[index];
+  }
+
+  _segment_sel(index: number) {
+    if (!this.swiper) { this.swiper_ready(); }
+    this.swiper?.slideTo(index)
   }
 
 }
